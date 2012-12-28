7ef7abbd69395426d248f891e97129bb69ef265d
Carlos Jiga <c.jiga1983@gmail.com>
2012-12-28 00:21:55 -0500

    commit

--- src/main/java/com/axcessfinancial/springapp/dao/ProductDao.java
+++ src/main/java/com/axcessfinancial/springapp/dao/ProductDao.java
@@ -1,5 +1,13 @@
 package com.axcessfinancial.springapp.dao;
 
+import java.util.List;
+
+import com.axcessfinancial.springapp.domain.Product;
+
 public interface ProductDao {
+
+	public List<Product> getProductList();
+
+	public void saveProduct(Product prod);
 
 }
